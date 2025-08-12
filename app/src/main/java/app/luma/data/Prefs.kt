0056b36f325163dcccc8935d4129f955afb9a674
vandamd <vandamdinh71@gmail.com>
2025-08-12 20:41:37 +0100

    feat: increase limit to 5 pages

--- app/src/main/java/app/luma/data/Prefs.kt
+++ app/src/main/java/app/luma/data/Prefs.kt
@@ -123,7 +123,7 @@ class Prefs(val context: Context) {
                 1
             }
         }
-        set(value) = prefs.edit().putInt(HOME_PAGES, value.coerceIn(1, 3)).apply()
+        set(value) = prefs.edit().putInt(HOME_PAGES, value.coerceIn(1, 5)).apply()
 
     fun getAppsPerPage(page: Int): Int {
         return try {
