b3b3ad8d5f2f467854343e57451f8a621c4f3580
vandamd <vandamdinh71@gmail.com>
2026-02-10 21:16:57 +0000

    disable haptic on page turn

--- app/src/main/java/app/luma/ui/HomeFragment.kt
+++ app/src/main/java/app/luma/ui/HomeFragment.kt
@@ -910,7 +910,6 @@ class HomeFragment :
 
     private fun handleSwipeUp() {
         if (totalPages > 1 && currentPage < totalPages - 1) {
-            performHapticFeedback(requireContext())
             switchToPage(currentPage + 1)
         } else {
             handleGesture(GestureType.SWIPE_UP)
@@ -919,7 +918,6 @@ class HomeFragment :
 
     private fun handleSwipeDown() {
         if (totalPages > 1 && currentPage > 0) {
-            performHapticFeedback(requireContext())
             switchToPage(currentPage - 1)
         } else {
             handleGesture(GestureType.SWIPE_DOWN)
